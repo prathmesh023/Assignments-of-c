e12d99782cfde1b51147b5904d7cc3825c6de80c
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2025-09-26 15:32:00 +0530

    Problems on Numbers

---
+++ 1console.c
@@ -0,0 +1,13 @@
+//Write a program which print sentence on console
+
+#include <stdio.h>
+int main() 
+{
+
+    printf("HIE,this is a printed a sentens on the console.\n");
+
+    return 0;
+}
+
+// Output=
+// HIE,this is a printed a sentens on the console.
