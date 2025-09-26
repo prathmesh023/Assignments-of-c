ffc6cd807761c3e1aacce1c27759b264d9729a60
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2025-09-26 15:39:03 +0530

    Problems on Pattern

---
+++ 1pattern*.c
@@ -0,0 +1,21 @@
+/* 1. Accept number of elements to be print from user and print following pattern
+* * * * * * */
+
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter number of elements (n): ");
+    scanf("%d", &n);
+    
+    printf("Pattern: \n");
+    for (i = 0; i < n; i++) {
+        printf("* ");
+    }
+    printf("\n");
+    return 0;
+}
+/*
+Output :
+Enter number of elements (n): 6
+Pattern: 
+* * * * * * */
